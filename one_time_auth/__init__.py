65347422cfab5c08fc231c366fe586f5e53ba6b5
Rahul D Shetty <35rahuldshetty@gmail.com>
2023-01-13 14:28:48 +0530

    Update __init__.py

--- one_time_auth/__init__.py
+++ one_time_auth/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Rahul D Shetty"""
 __email__ = '35rahuldshetty@gmail.com'
 __version__ = '0.1.0'
 
-from one_time_auth.one_time_auth import OneTimeAuthenticator
+from one_time_auth.one_time_auth import OneTimeAuthenticator, UserTokenAuthenticator
